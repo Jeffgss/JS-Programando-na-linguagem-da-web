69d9effa63c6a896c9fe938b50f26e792588b14c
Jeff Santos <jefferson.santos@maxmilhas.com.br>
2021-05-25 14:26:32 -0300

    2-Variables and Operators

--- js/index.js
+++ js/index.js
@@ -1,2 +1,30 @@
 let titulo = document.querySelector(".title");
 titulo.textContent = "Aparecida Nutricionista";
+
+let paciente = document.querySelector("#primeiro-paciente");
+
+let tdPeso = paciente.querySelector(".info-peso");
+let peso = tdPeso.textContent;
+
+let tdAltura = paciente.querySelector(".info-altura");
+let altura = tdAltura.textContent;
+
+let tdImc = paciente.querySelector(".info-imc");
+
+let pesoEValido = true;
+let alturaEValida = true;
+
+if (peso <= 0 || peso >= 597) {
+   pesoEValido = false;
+   tdImc.textContent = "Peso inválido";
+}
+
+if (altura <= 0 || altura >= 2.74) {
+   alturaEValida = false;
+   tdImc.textContent = "Altura inválida";
+}
+
+if (pesoEValido && alturaEValida) {
+   let imc = peso / (altura * altura);
+   tdImc.textContent = imc;
+}
